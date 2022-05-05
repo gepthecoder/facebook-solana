839e29ca43d7cfb446087425b686b12555a3b71d
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-05 18:37:29 +0200

    $0005: [Rust]: post account structure

--- facebook-solana/programs/programs/programs/src/lib.rs
+++ facebook-solana/programs/programs/programs/src/lib.rs
@@ -11,5 +11,43 @@ pub mod programs {
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[derive(Account)]
+pub struct CreateState<'info> {
+    #[account(
+        init,
+        seeds = [b"state".as_ref()], //generate random seed in this state variable
+        bump, //make a block unique
+        payer = authority,
+        space = size_of::<StateAccount>() + 8
+    )]
+    pub state: Account<'info StateAccount>,
+
+    // Authority (this is signer who paid transaction fee); "mut as mutable-changable"
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    // System program
+    pub system.program: UncheckedAccount<'info>, // make this part more secure in production
+
+    // Token program
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+}
+
+// Post Account Structure
+#[account]
+pub struct PostAccount {
+    pub authority: Pubkey,
+
+    pub text: String,
+
+    pub poster_name: String,
+
+    pub poster_url: String,
+
+    pub comment_count: u64,
+
+    pub index: u64,
+
+    pub post_time: i64,
+}
